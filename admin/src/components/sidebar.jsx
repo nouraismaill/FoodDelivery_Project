34bd7f060973492a11d232b8b90596ee3cf7beaa
Noura Ismail <12030827@students.liu.edu.lb>
2024-08-07 22:23:46 +0300

    Add SideBar

--- admin/src/components/sidebar.jsx
+++ admin/src/components/sidebar.jsx
@@ -1,37 +1,57 @@
 import React from "react";
+
+import { NavLink } from "react-router-dom";
 import { assets } from "../assets/assets";
+
 const Sidebar = () => {
   return (
-    <div class="relative flex h-[calc(100vh-0rem)] w-full max-w-[20rem] flex-col rounded-xl bg-white bg-clip-border p-4 text-gray-700 shadow-xl shadow-blue-gray-900/5">
-      <nav class="flex min-w-[240px] flex-col gap-1 p-2 font-sans text-base font-normal text-blue-gray-700">
-        <div
-          role="button"
-          class="flex items-center w-full p-3 leading-tight transition-all rounded-lg outline-none text-start hover:bg-blue-gray-50 hover:bg-opacity-80 hover:text-blue-gray-900 focus:bg-blue-gray-50 focus:bg-opacity-80 focus:text-blue-gray-900 active:bg-blue-gray-50 active:bg-opacity-80 active:text-blue-gray-900"
+    <div class="relative w-18%  flex min-h-[100vh]  flex-col rounded-xl bg-white bg-clip-border p-4 text-gray-700 shadow-xl shadow-blue-gray-900/5 border-t-0">
+      <nav class="flex flex-col gap-1 p-2 font-sans text-base font-normal text-blue-gray-700   sm:min-w-7 ">
+        <NavLink
+          to="/add"
+          className={({ isActive }) =>
+            isActive
+              ? "bg-[#fff0ed] border-red-500 "
+              : "flex items-center w-full p-3 leading-tight transition-all rounded-lg outline-none text-start hover:bg-blue-gray-50 hover:bg-opacity-80 hover:text-blue-gray-900 focus:bg-blue-gray-50 focus:bg-opacity-80 focus:text-blue-gray-900 active:bg-blue-gray-50 active:bg-opacity-80 active:text-blue-gray-900"
+          }
         >
-          <div class="grid mr-4 place-items-center">
-            <img src={assets.add_icon} alt="" />
+          <div class="flex items-center w-full  p-3 leading-tight transition-all rounded-lg outline-none text-start hover:bg-blue-gray-50 hover:bg-opacity-80 hover:text-blue-gray-900 focus:bg-blue-gray-50 focus:bg-opacity-80 focus:text-blue-gray-900 active:bg-blue-gray-50 active:bg-opacity-80 active:text-blue-gray-900">
+            <div class="grid mr-4 place-items-center">
+              <img src={assets.add_icon} alt="" />
+            </div>
+            Add Items
           </div>
-          Add Items
-        </div>
-        <div
-          role="button"
-          class="flex items-center w-full p-3 leading-tight transition-all rounded-lg outline-none text-start hover:bg-blue-gray-50 hover:bg-opacity-80 hover:text-blue-gray-900 focus:bg-blue-gray-50 focus:bg-opacity-80 focus:text-blue-gray-900 active:bg-blue-gray-50 active:bg-opacity-80 active:text-blue-gray-900"
+        </NavLink>
+        <NavLink
+          to="/list"
+          className={({ isActive }) =>
+            isActive
+              ? "bg-[#fff0ed] border-red-500 "
+              : "flex items-center w-full p-3 leading-tight transition-all rounded-lg outline-none text-start hover:bg-blue-gray-50 hover:bg-opacity-80 hover:text-blue-gray-900 focus:bg-blue-gray-50 focus:bg-opacity-80 focus:text-blue-gray-900 active:bg-blue-gray-50 active:bg-opacity-80 active:text-blue-gray-900"
+          }
         >
-          <div class="grid mr-4 place-items-center">
-            <img src={assets.order_icon} alt="" />
+          <div class="flex items-center w-full p-3 leading-tight transition-all rounded-lg outline-none text-start hover:bg-blue-gray-50 hover:bg-opacity-80 hover:text-blue-gray-900 focus:bg-blue-gray-50 focus:bg-opacity-80 focus:text-blue-gray-900 active:bg-blue-gray-50 active:bg-opacity-80 active:text-blue-gray-900">
+            <div class="grid mr-4 place-items-center">
+              <img src={assets.order_icon} alt="" />
+            </div>
+            List Items
           </div>
-          List Items
-        </div>
-
-        <div
-          role="button"
-          class="flex items-center w-full p-3 leading-tight transition-all rounded-lg outline-none text-start hover:bg-blue-gray-50 hover:bg-opacity-80 hover:text-blue-gray-900 focus:bg-blue-gray-50 focus:bg-opacity-80 focus:text-blue-gray-900 active:bg-blue-gray-50 active:bg-opacity-80 active:text-blue-gray-900"
+        </NavLink>
+        <NavLink
+          to="/orders"
+          className={({ isActive }) =>
+            isActive
+              ? "bg-[#fff0ed] border-red-500 "
+              : "flex items-center w-full p-3 leading-tight transition-all rounded-lg outline-none text-start hover:bg-blue-gray-50 hover:bg-opacity-80 hover:text-blue-gray-900 focus:bg-blue-gray-50 focus:bg-opacity-80 focus:text-blue-gray-900 active:bg-blue-gray-50 active:bg-opacity-80 active:text-blue-gray-900"
+          }
         >
-          <div class="grid mr-4 place-items-center">
-            <img src={assets.order_icon} alt="" />
+          <div class="flex items-center w-full p-3 leading-tight transition-all rounded-lg outline-none text-start hover:bg-blue-gray-50 hover:bg-opacity-80 hover:text-blue-gray-900 focus:bg-blue-gray-50 focus:bg-opacity-80 focus:text-blue-gray-900 active:bg-blue-gray-50 active:bg-opacity-80 active:text-blue-gray-900">
+            <div class="grid mr-4 place-items-center">
+              <img src={assets.order_icon} alt="" />
+            </div>
+            Orders
           </div>
-          Orders
-        </div>
+        </NavLink>
       </nav>
     </div>
   );
