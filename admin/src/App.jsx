bbdb8134987478480aa85685e787c08a04e4b8a4
Noura Ismail <12030827@students.liu.edu.lb>
2024-08-07 22:24:08 +0300

    Add Routes

--- admin/src/App.jsx
+++ admin/src/App.jsx
@@ -1,13 +1,22 @@
 import React from "react";
+import { Route, Routes } from "react-router-dom";
 import Navbar from "./components/Navbar";
 import Sidebar from "./components/sidebar";
+import Add from "./pages/Add";
+import List from "./pages/List";
+import Orders from "./pages/Orders";
 const App = () => {
   return (
     <div>
       <Navbar />
       <hr />
-      <div className="flex ">
+      <div className="flex">
         <Sidebar />
+        <Routes>
+          <Route path="/add" element={<Add />} />
+          <Route path="/list" element={<List />} />
+          <Route path="/orders" element={<Orders />} />
+        </Routes>
       </div>
     </div>
   );
