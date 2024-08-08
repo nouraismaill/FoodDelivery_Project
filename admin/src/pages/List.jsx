847dd38740d6177d8addfb2907447bc049ea7b9b
Noura Ismail <12030827@students.liu.edu.lb>
2024-08-09 01:32:23 +0300

    add tables

--- admin/src/pages/List.jsx
+++ admin/src/pages/List.jsx
@@ -1,7 +1,76 @@
-import React from "react";
+import axios from "axios";
+import React, { useEffect, useState } from "react";
+import { toast } from "react-toastify";
 
 const List = () => {
-  return <div>List</div>;
+  const [list, setList] = useState([]);
+  const url = "http://localhost:3000";
+
+  const fetchList = async () => {
+    const respo = await axios.get(`${url}/api/food/list`);
+    console.log(respo.data);
+    if (respo.data.success) {
+      setList(respo.data.data);
+    } else {
+      toast.error("Error");
+    }
+  };
+  const deleteItems = async () => {
+    const response = await axios.post(`${url}/api/food/delete`);
+  };
+
+  useEffect(() => {
+    fetchList();
+  }, []);
+
+  return (
+    <div className="w-full my-7 px-4 md:px-8">
+      <div className="w-full">
+        <p className="text-gray-800 text-xl font-bold sm:text-2xl">
+          All Food List
+        </p>
+      </div>
+      <div className="mt-6 shadow-sm border rounded-lg overflow-x-auto w-full">
+        <table className="w-full table-auto text-sm text-left">
+          <thead className="bg-gray-50 text-gray-600 font-medium border-b hidden lg:table-header-group md:table-header-group ">
+            <tr>
+              <th className="py-3 px-6">Image</th>
+              <th className="py-3 px-6">Name</th>
+              <th className="py-3 px-6">Category</th>
+              <th className="py-3 px-6">Price</th>
+              <th className="py-3 px-6">Action</th>
+            </tr>
+          </thead>
+          <tbody className="text-gray-600 divide-y">
+            {list.map((item, idx) => (
+              <tr
+                key={idx}
+                className="flex flex-col sm:table-row md:table-row "
+              >
+                <td className="px-6 py-4 whitespace-nowrap flex items-center justify-center sm:justify-start">
+                  <img
+                    className="lg:w-[50px] md:w-[50px] sm:w-fit"
+                    src={`${url}/images/` + item.image}
+                    alt={item.name}
+                  />
+                </td>
+                <td className="px-6 py-4 whitespace-nowrap sm:px-1">
+                  {item.name}
+                </td>
+                <td className="px-6 py-4 whitespace-nowrap">{item.category}</td>
+                <td className="px-6 py-4 whitespace-nowrap">${item.price}</td>
+                <td className="px-6 py-4 whitespace-nowrap">
+                  <button className="text-red-600" onClick={deleteItems}>
+                    Delete
+                  </button>
+                </td>
+              </tr>
+            ))}
+          </tbody>
+        </table>
+      </div>
+    </div>
+  );
 };
 
 export default List;
